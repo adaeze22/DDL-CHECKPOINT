e698204662d203345ccb2731f9cbc7943eec1f0d
adaeze22 <115624160+adaeze22@users.noreply.github.com>
2022-11-09 10:43:52 +0100

    DDL

---
+++ ddl checkpoint.sql
@@ -0,0 +1,22 @@
+create table  PRODUCT (
+   Product_id varchar(20) constraint product_pkey Primary key,
+	Product_Name varchar(20) constraint product_notnull not null,
+	Price int constraint price_check check (price > 0)
+);
+
+
+
+create table CUSTOMER (
+   Customer_id varchar(20) constraint customer_pkey Primary key,
+	Customer_name varchar(20) constraint customer_notnull not null,
+	Customer_Tel int
+);
+
+
+
+create table Orders (
+   Quantity int,
+	Total_amount int
+	CONSTRAINT orders_fkey1 FOREIGN KEY (Customer_id) REFERENCES CUSTOMER (Customer_id),
+	CONSTRAINT orders_fkey2 FOREIGN KEY (Product_id) REFERENCES PRODUCT (Product_id)
+)
