af0edb44b4f6da0fcce8be976736b4d8921ca90c
adaeze22 <115624160+adaeze22@users.noreply.github.com>
2022-11-16 09:43:44 +0100

    Update ddl checkpoint.sql

--- ddl checkpoint.sql
+++ ddl checkpoint.sql
@@ -15,8 +15,15 @@ create table CUSTOMER (
 
 
 create table Orders (
-   Quantity int,
+	customer_id varchar(20),
+	product_id varchar(20),
+     Quantity int,
 	Total_amount int
 	CONSTRAINT orders_fkey1 FOREIGN KEY (Customer_id) REFERENCES CUSTOMER (Customer_id),
 	CONSTRAINT orders_fkey2 FOREIGN KEY (Product_id) REFERENCES PRODUCT (Product_id)
-)
+);
+
+
+alter table product add  Category VARCHAR2(20);
+
+alter table ORDERS add  OrderDate DATE default (current_date);
